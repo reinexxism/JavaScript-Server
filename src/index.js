c71888b845f63c4720e3309ed5160a3417abb81c
reinexxism <lucy0805@naver.com>
2024-01-27 18:45:58 +0900

    GraphQL 스키마, 리졸버 함수 작성 완료

--- src/index.js
+++ src/index.js
@@ -1,2 +1,60 @@
-// index.js
-// This is the main entry point of our application
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const port = process.env.PORT || 4000;
+
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Joohyun' },
+  { id: '2', content: 'This is another note', author: 'Dahee' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Nanyoung' }
+];
+
+const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+
+  type Query {
+    hello: String!
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+
+  type Mutation {
+    newNote(content: String!): Note!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello, My name is Joohyun',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id);
+    }
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Joohyun'
+      };
+      notes.push(noteValue);
+      return noteValue;
+    }
+  }
+};
+
+const app = express();
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+  )
+);
