f974e3403bac658ef12e1c799412e4414c93c400
reinexxism <lucy0805@naver.com>
2024-01-30 18:59:00 +0900

    Refactoring: Express/Apollo server 코드 분리

--- src/index.js
+++ src/index.js
@@ -1,56 +1,26 @@
 const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 require('dotenv').config();
 
 const db = require('./db');
 const models = require('./models');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 
 const port = process.env.PORT || 4000;
 const DB_HOST = process.env.DB_HOST;
-
-const typeDefs = gql`
-  type Note {
-    id: ID!
-    content: String!
-    author: String!
-  }
-
-  type Query {
-    hello: String!
-    notes: [Note!]!
-    note(id: ID!): Note!
-  }
-
-  type Mutation {
-    newNote(content: String!): Note!
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hello: () => 'Hello, My name is Joohyun',
-    notes: async () => {
-      return await models.Note.find();
-    },
-    note: async (parent, args) => {
-      return await models.Note.findById(args.id);
-    }
-  },
-  Mutation: {
-    newNote: async (parent, args) => {
-      return await models.Note.create({
-        content: args.content,
-        author: 'Joohyun'
-      });
-    }
-  }
-};
 
 const app = express();
 
 db.connect(DB_HOST);
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: () => {
+    return { models };
+  }
+});
 server.applyMiddleware({ app, path: '/api' });
 
 app.listen({ port }, () =>
